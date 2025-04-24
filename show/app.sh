7ce163d64dbb9fc651eb779f4cd02a9706d8e02f
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2025-04-24 10:57:26 +0000

    Update show

--- show/app.sh
+++ show/app.sh
@@ -7,8 +7,7 @@ pg
 cpg
 reason show < edit.txt | log > show.txt
 native $lang < show.txt | log > native.txt
-sens < native.txt | log > sens.txt
-addn < sens.txt | log > content.txt
+fmts < native.txt | log > content.txt
 image < show.txt | log > image.txt
 curl -s -o image.png "$(cat image.txt)"
 fb --media image.png < content.txt
