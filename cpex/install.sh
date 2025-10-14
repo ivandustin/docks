5acda07eaf38162a9b787838c152590231e23f75
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2025-10-14 10:43:17 +0800

    Fix cpex

--- cpex/install.sh
+++ cpex/install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -euvxo pipefail
 pip install playwright chevron
-playwright install chromium
-playwright install-deps
+playwright install --with-deps
 git clone https://github.com/ivandustin/sdk.git
 git clone https://github.com/ivandustin/bible.git
